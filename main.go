e9fdbb993e1175bed1f09383f8ec257894d25079
BPplays <suzuko.oss@proton.me>
2025-05-18 18:56:28 -0700

    test idk?

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"crypto/sha512"
 	"crypto/subtle"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
 	"os"
@@ -15,11 +18,13 @@ import (
 	"strings"
 	"time"
 
-	"github.com/spf13/pflag"
 	clipboard "github.com/dece2183/go-clipboard"
+	"github.com/spf13/pflag"
+	"golang.org/x/text/message"
+
+	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 type ime_req struct {
@@ -81,12 +86,14 @@ func main() {
 	// Define flags
 	var ipAddr string
 	var im_message string
+	var endpoint string
 	var password string
 	var isServer bool
 	var port int
 
 	pflag.StringVarP(&ipAddr, "ip", "i", "", "IP address to send the UTF-8 string to")
 	pflag.StringVarP(&im_message, "message", "m", "", "UTF-8 string to send")
+	pflag.StringVarP(&endpoint, "endpoint", "n", "", "UTF-8 string to send")
 	pflag.StringVarP(&password, "password", "p", "", "Password (hashed with SHA-512)")
 	pflag.BoolVarP(&isServer, "receive", "d", false, "Receive UTF-8 string over TCP")
 	pflag.BoolVarP(&debug, "debug", "e", false, "Receive UTF-8 string over TCP")
@@ -108,12 +115,21 @@ func main() {
 		if im_message == "" {
 			get_im = true
 			// im_message = get_im_str
+			makeGetRequest()
 		}
 		if password == "" {
 			fmt.Println("Please provide a password with -p.")
 			os.Exit(1)
 		}
-		sendToIP(ipAddr, im_message, password, port)
+		url := "http://localhost:32512/current_ime" // Your POST URL
+		username := "admin"                        // Your username
+		data := map[string]interface{}{
+			"IME":       "value",
+			"IME_value": im_message,
+		}
+
+		makePostRequest(url, username, password, data)
+
 	} else {
 		fmt.Println("Please provide either an IP address with -i or use -d to receive data.")
 	}
@@ -160,6 +176,70 @@ func parse_im(msg *string) {
 		}
 	}
 
+}
+
+func makePostRequest(url string, username string, password string, data interface{}) {
+	// Encode the data into JSON format
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a new request with Basic Authentication
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Set the Content-Type header
+	req.Header.Set("Content-Type", "application/json")
+
+	// Set Basic Authentication header
+	req.SetBasicAuth(username, password)
+
+	// Perform the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	// Read and print the response body
+	body := make([]byte, resp.ContentLength)
+	_, err = resp.Body.Read(body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Response Body:", string(body))
+}
+
+
+func makeGetRequest(url string) string {
+	// Send a GET request
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Define a variable to hold the parsed JSON
+	var response ime_req
+
+	// Parse the JSON response into the Response struct
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Return the IME_value from the response struct
+	return response.IME_value
 }
 
 func isIPv6(address string) bool {
@@ -296,7 +376,7 @@ func startServer(expectedPassword string, port int) {
 			return
 		}
 
-		im_map[reqData.IME] = reqData.IME_value
+		parse_im(&reqData.IME_value)
 		fmt.Println(im_map)
 		switch_im()
 
@@ -329,219 +409,11 @@ func set_clipboard(str string) {
 }
 
 
-// const get_im_str string = "mode:getim"
-// const set_im_str string = "set_modes"
-
-
-
-// func handleConnection(conn net.Conn, expectedPassword string) error {
-// 	var get_im bool = false
-// 	defer conn.Close()
-// 	reader := bufio.NewReader(conn)
-//
-// 	// Read and verify password
-// 	log.Println("Waiting to read password...")
-// 	passwordHash, err := readUntilNull(reader)
-// 	if err != nil {
-// 		return fmt.Errorf("error reading password: %v\n", err)
-// 	}
-//
-//
-// 	log.Printf("Received password hash: %s\n", passwordHash)
-// 	if secCompareStrings(passwordHash, expectedPassword) != true {
-// 		fmt.Printf("expected pass: %v\n", expectedPassword)
-// 		return fmt.Errorf("password verification failed")
-// 	}
-//
-// 	// Process the actual message
-// 	for {
-// 		log.Println("Waiting to read message...")
-// 		conn.Write(str2nulbs(send_pass_str))
-// 		message, err := readUntilNull(reader)
-// 		if err != nil {
-// 			if err.Error() != "EOF" {
-// 				return fmt.Errorf("error reading from connection: %v", err)
-// 			}
-// 			return nil
-// 		}
-// 		response := "Message processed successfully\n"
-//
-// 		log.Printf("Received message: %s", message)
-// 		if message == get_im_str {
-// 			get_im = true
-// 		}
-// 		spl_msg := strings.Split(message, ":")
-// 		if get_im {
-// 			response = ret_im()
-// 		} else if spl_msg[0] == set_im_str {
-// 			parse_im(&spl_msg[1])
-// 			fmt.Println(im_map)
-// 			switch_im()
-// 		}
-//
-// 		// // Send response back to client
-// 		// _, err = conn.Write(str2nulbs(send_msg_str))
-// 		// if err != nil {
-// 		// 	log.Println(err)
-// 		// }
-//
-// 		_, err = conn.Write(str2nulbs(response))
-// 		if err != nil {
-// 			return fmt.Errorf("error sending response: %v", err)
-// 		}
-// 	}
-// }
-
-
-
-func readUntilNull(reader *bufio.Reader) (string, error) {
-	var result strings.Builder
-	for {
-		b, err := reader.ReadByte()
-		if err != nil {
-			return "", err
-		}
-		if b == null_b {
-			break
-		}
-		result.WriteByte(b)
-	}
-	return result.String(), nil
-}
-
-
-const null_b byte = '\u0000'
-var null_bsl []byte = []byte{0}
-
-
-func str2nulbs(s string) []byte {
-	b := []byte(s)
-	b = append(b, null_b)
-	return b
-}
-
-
-
-const send_pass_str string = "com:sendpass"
-const send_msg_str string = "com:sendmsg"
-
-
-func sendToIP(ipAddr string, message string, password string, port int) {
-	if isIPv6(ipAddr) {
-		var err error
-		ipAddr, err = FormatIPv6(ipAddr)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-	}
-	dialer := &net.Dialer{Timeout: time.Duration(timeout) * time.Millisecond}
-	conn, err := dialer.Dial("tcp", ipAddr+":"+strconv.Itoa(port))
-	if err != nil {
-		log.Fatalf("Error connecting to %s:%d: %v", ipAddr, port, err)
-	}
-	defer conn.Close()
-
-	// Send password hash
-	passwordHash := hashPassword(password)
-	if debug {
-		fmt.Println("sending pass")
-	}
-	_, err = conn.Write(str2nulbs(passwordHash))
-	if err != nil {
-		log.Fatalf("Error sending password hash: %v", err)
-	}
-
-	// Set a read deadline for receiving the 'send_pass_str' from the server
-	conn.SetReadDeadline(time.Now().Add(time.Duration(timeout) * time.Millisecond))
-
-	// Read response from the server and wait for 'send_pass_str'
-	if debug {
-		fmt.Println("waiting for 'send_pass_str'")
-	}
-	bufReader := bufio.NewReader(conn)
-	response, err := readUntilNull(bufReader)
-	if err != nil {
-		if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-			log.Fatalf("Timeout waiting for server response: %v", err)
-		} else {
-			log.Fatalf("Error reading response: %v", err)
-		}
-	}
-	if response != send_pass_str {
-		log.Fatalf("Unexpected server response: %s", response)
-	}
 
 
 
 
 
-
-
-
-
-	// Send message
-	if debug {
-		fmt.Println("sending msg")
-	}
-	_, err = conn.Write(str2nulbs(message))
-	if err != nil {
-		log.Fatalf("Error sending data: %v", err)
-	}
-
-
-
-
-
-	// // Set a read deadline for receiving the 'send_pass_str' from the server
-	// conn.SetReadDeadline(time.Now().Add(time.Duration(timeout) * time.Millisecond))
-	//
-	// // Read response from the server and wait for 'send_pass_str'
-	// if debug {
-	// 	fmt.Println("waiting for 'send_msg_str'")
-	// }
-	// bufReader = bufio.NewReader(conn)
-	// response, err = readUntilNull(bufReader)
-	// if err != nil {
-	// 	if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-	// 		log.Fatalf("Timeout waiting for server response: %v", err)
-	// 	} else {
-	// 		log.Fatalf("Error reading response: %v", err)
-	// 	}
-	// }
-	// if response != send_msg_str {
-	// 	log.Fatalf("Unexpected server response: %s", response)
-	// }
-
-
-
-
-
-
-
-	// Set a read deadline for receiving a response
-	conn.SetReadDeadline(time.Now().Add(time.Duration(timeout) * time.Millisecond))
-
-	// Read response from the server
-	if debug {
-		fmt.Println("recv resp")
-	}
-	response, err = bufio.NewReader(conn).ReadString(null_b)
-	if err != nil {
-		if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-			log.Fatalf("Timeout waiting for server response: %v", err)
-		} else {
-			log.Fatalf("Error reading response: %v", err)
-		}
-	}
-	if debug {
-		fmt.Printf("Server response: %s", response)
-	}
-
-	if get_im {
-		fmt.Print(response)
-	}
-}
 
 func hashPassword(password string) string {
 	hash := sha512.New()
